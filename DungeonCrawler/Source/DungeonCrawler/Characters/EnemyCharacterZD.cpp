1374e3a026a05323caa40c7016aacbfee1a037e9
Emerson <muondove@gmail.com>
2025-01-05 17:14:57 +0200

    base class for enemy ZD

---
+++ DungeonCrawler/Source/DungeonCrawler/Characters/EnemyCharacterZD.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyCharacterZD.h"
+
+
+// Sets default values
+AEnemyCharacterZD::AEnemyCharacterZD()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEnemyCharacterZD::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyCharacterZD::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AEnemyCharacterZD::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
